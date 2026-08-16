9706c59dc67d461ec42be388c3428a2277bc1d56
Kris Lunde <hello@krislunde.com>
2026-08-16 15:38:23 +0200

    Added production and development environments, plus next.js

---
+++ apps/studio/sanity.cli.ts
@@ -0,0 +1,25 @@
+import { defineCliConfig } from 'sanity/cli'
+
+export default defineCliConfig({
+    api: {
+        projectId: process.env.SANITY_STUDIO_PROJECT_ID!,
+        dataset: process.env.SANITY_STUDIO_DATASET!,
+    },
+
+    studioHost: process.env.SANITY_STUDIO_HOSTNAME!,
+
+    deployment: {
+        autoUpdates: true,
+    },
+
+    server: {
+        port: 3334,
+    },
+
+    typegen: {
+        enabled: true,
+        path: '../web/src/**/*.{ts,tsx,js,jsx}',
+        schema: './schema.json',
+        generates: '../web/src/sanity/types.ts',
+    },
+})
